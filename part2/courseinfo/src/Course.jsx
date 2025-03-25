a46c436245eb0b4c819fc8aa049682a998c2e2ba
mrhonneynive <38505964+mrhonneynive@users.noreply.github.com>
2025-03-26 01:34:08 +0300

    Complete exercise 2.1

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -2,8 +2,15 @@ const Course = ({ course }) => {
   return (
     <div>
       <h1>{course.name}</h1>
-    </div> 
+      {course.parts.map((part) => {
+        return (
+          <p key={part.id}>
+            {part.name} {part.exercises}
+          </p>
+        );
+      })}
+    </div>
   );
-}
+};
 
 export default Course;
